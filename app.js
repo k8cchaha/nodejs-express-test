ebe10c19ce3b3aa282db3f433786c47946638235
Alex_Wang <k8cchaha@gmail.com>
2022-05-05 15:10:22 +0800

    separate routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const user = require("./routes/user");
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello");
+// });
+
+app.use("/user", user);
 
 app.listen(3111);
